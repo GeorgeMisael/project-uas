31f68f1bd5236128dda1d8f725e29f28b9d7ed4f
George Misael <georgemisaelgantume@gmail.com>
2024-08-26 21:11:36 +0700

    Menambahkan isi file mahasiswa

--- resources/views/login/mahasiswa.blade.php
+++ resources/views/login/mahasiswa.blade.php
@@ -4,9 +4,16 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Mahasiswa</title>
 </head>
 <body>
+  <h1>Mahasiswa</h1>
+
+  <ul>
+    <li>George Misael Gantume : 434231115</li>
+    <li>Mikael Kristian : 434231117</li>
+  </ul>
   
+  <p>Selesai</p>
 </body>
 </html>
